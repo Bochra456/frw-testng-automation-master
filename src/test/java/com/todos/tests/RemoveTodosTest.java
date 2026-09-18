acdf4f8cc53f2a649720875ad9b8b4166560884f
Bochra456 <bochralamiri22@gmail.com>
2026-09-18 22:51:55 +0100

    RemoveTodosTest.java

--- src/test/java/com/todos/tests/RemoveTodosTest.java
+++ src/test/java/com/todos/tests/RemoveTodosTest.java
@@ -19,16 +19,15 @@ package com.todos.tests;
 	    public void iCanRemoveTodos() throws IOException {
 	        todosPage = new TodosPage();
 
-	        // Étape 1 : ajouter une tâche à supprimer
+	        
 	        todosPage.submitTodos(prop.getProperty("todos1"));
 
 	        String elementTodo = todosPage.checkElementContain(TodosPage.todoText);
 	        assertTrue(elementTodo.contains(prop.getProperty("todos1")));
 
-	        // Étape 2 : supprimer la tâche
 	        todosPage.removeTodo();
 
-	        // Étape 3 : vérifier que la liste est vide
+	    
 	        boolean isEmpty = todosPage.isTodoListEmpty();
 	        assertTrue(isEmpty);
 	    }
